b74b96972f985e5b1cea1d7ebf7b492028dbb269
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-05-03 13:18:45 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import "../styles.css";
 import "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BsFillBagFill } from 'react-icons/bs'; 
 import {Navbar} from "react-bootstrap";
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
+
 class Header extends Component{
     logout(){
         localStorage.removeItem("token");
     }
+
+    moveHome(){
+        history.push("/cart");
+        window.location.reload();
+    }
+
     render(){
         return(
             <div>
                 <Navbar bg="white" expand="lg">
-                    <img src={require("../images/Harvest Logo.png")} className="logoHome" alt="Logo"></img>       
+                    <img src={require("../images/Harvest Logo.png")} className="logoHome" alt="Logo"></img> 
+                      <BsFillBagFill className="bagicon" size='26' onClick={()=>{this.moveHome()}}/>    
                 </Navbar> 
                 <div >
                    <div className="allmenu" >
