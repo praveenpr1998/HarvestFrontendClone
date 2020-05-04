22c0ae2c91cfa03e30556d513d21280eff5d659a
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-05-04 13:44:11 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import "../styles.css";
 import "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BsFillBagFill } from 'react-icons/bs'; 
-import {Navbar} from "react-bootstrap";
+import {Navbar,Nav} from "react-bootstrap";
 import { createBrowserHistory } from "history";
 const history = createBrowserHistory();
 
@@ -20,21 +19,24 @@ class Header extends Component{
     render(){
         return(
             <div>
-                <Navbar bg="white" expand="lg">
-                    <img src={require("../images/Harvest Logo.png")} className="logoHome" alt="Logo"></img> 
-                      <BsFillBagFill className="bagicon" size='26' onClick={()=>{this.moveHome()}}/>    
-                </Navbar> 
-                <div >
-                   <div className="allmenu" >
-                     <a className="menu" href="/home">Home</a>
-                    <a href="/cart" className="logout" >My Cart</a>
-                    <a href="/"  className="logout" >Logout</a>
-                    <a href="/contact" className="logout" >Contact</a>
-                    </div> 
-                </div>
-                <div style={{height:10,width:'100%',backgroundColor:'#dbdbdb',position:'absolute',left:0,right:0}}>
-                </div>
-            </div>
+            <Navbar bg="white" expand="lg">
+            <Navbar.Brand href="#home">
+            <img src={require("../images/Harvest Logo.png")} className="logoHome" alt="Logo"></img> 
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Link href="/home"><p className="tabmenus">Home</p></Nav.Link>
+                <Nav.Link href="/cart"><p className="tabmenus">MyCart</p></Nav.Link>
+                <Nav.Link onClick={()=>this.logout()}  href="/"><p className="tabmenus">Logout</p></Nav.Link>
+                <Nav.Link href="/contact"><p className="tabmenus">Contact</p></Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        <div style={{height:10,width:'100%',backgroundColor:'#dbdbdb',position:'absolute',left:0,right:0}}>
+         </div>
+         </div>
+            
         )
     }
 }
