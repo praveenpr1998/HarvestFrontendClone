14cb9e40b169bc53422effed180cdbb3572e9589
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-05-08 12:18:39 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,16 +12,17 @@ class Header extends Component{
   state={
     token:''
   }
-    async logout(){
-      this.setState({token:localStorage.getItem("token")},()=>{
+     async logout(){
+      this.setState({token:await localStorage.getItem("token")},()=>{
         fetch(GLOBAL.BASE_URL+"users/removeUser",{
         method:"POST",
-        body:JSON.stringify({token:this.state.token}),
+        body:JSON.stringify({token:this.state.token})
     })
     .then(res => res.json())
     .then(
       (result) => { 
         if(result.message==="Success"){
+          console.log(result.message)
         localStorage.removeItem("token");
         history.push("/");
         window.location.reload();
@@ -34,41 +35,47 @@ class Header extends Component{
         return(
             <div>
             <Navbar bg="white" expand="lg">
-            <Navbar.Brand href="#home">
-            <img src={require("../Resources/Images/Harvest Logo.png")} className="logoHome" alt="Logo"></img> 
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-                <Nav.Link>
-                  <RouterNavLink
-                                    to='/home'
-                                    activeClassName=''
-                                    className='navLink-inactive'
-                                >
-                                    Home
-                                </RouterNavLink>
-                  </Nav.Link>
-                <Nav.Link >
                 <RouterNavLink
-                                    to='/cart'
-                                    activeClassName=''
-                                    className='navLink-inactive'
-                                >
-                                    My Cart
+                  to='/home'
+                  activeClassName=''
+                  className='navLink-inactive'
+                >
+                <Navbar.Brand href="">
+                  <img src={require("../Resources/Images/Harvest Logo.png")} className="logoHome" alt="Logo"></img> 
+                </Navbar.Brand>
                 </RouterNavLink>
-              </Nav.Link>
-                <Nav.Link onClick={()=>this.logout()}  >
-                <RouterNavLink
-                                    to='#'
-                                    activeClassName='navLink-active'
-                                    className='navLink-inactive'
-                                >
-                                    Logout
-                                </RouterNavLink>
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                      <Nav.Link>
+                        <RouterNavLink
+                          to='/home'
+                          activeClassName=''
+                          className='navLink-inactive'
+                         >
+                              Home
+                         </RouterNavLink>
+                      </Nav.Link>
+                      <Nav.Link >
+                        <RouterNavLink
+                            to='/cart'
+                            activeClassName=''
+                            className='navLink-inactive'
+                         >
+                              My Cart
+                        </RouterNavLink>
+                      </Nav.Link>
+                      <Nav.Link onClick={()=>this.logout()}  >
+                        <RouterNavLink
+                             to='#'
+                             activeClassName='navLink-active'
+                             className='navLink-inactive'
+                        >
+                           Logout
+                        </RouterNavLink>
+                      </Nav.Link>
+                    </Nav>
+                  </Navbar.Collapse>
           </Navbar>
         <div style={{height:10,width:'100%',backgroundColor:'#dbdbdb',position:'absolute',left:0,right:0}}>
          </div>
