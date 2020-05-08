0c7344bf29c2d405925dc060ae9524c415b22113
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-05-08 16:43:16 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,17 +2,20 @@ import React, { Component } from 'react';
 import Header from "../Components/Header.jsx"
 import "../styles.css";
 import {Card,Button,Spinner,Container} from "react-bootstrap";
-import { ToastContainer, toast } from 'react-toastify';
+import { AiOutlinePlusSquare } from 'react-icons/ai';
+import { AiOutlineMinusSquare } from 'react-icons/ai';
 const GLOBAL = require('../global');
 
 class Home extends Component{
     state={
         token:"",
         allProducts:[],
-        isLoading:''
+        isLoading:'',
+        addClicked:[],
+        quantity:[]
     }
     
-    componentDidMount(){
+     componentDidMount(){
         this.setState({isLoading:true})
         this.setState({token:localStorage.getItem("token")},()=>{
             fetch(GLOBAL.BASE_URL+"products/getProducts/",{
@@ -21,10 +24,21 @@ class Home extends Component{
         })
         .then(res => res.json())
         .then(
-          (result) => { 
+           async (result) => { 
             if(result.message==="Success"){
                 this.setState({isLoading:false})
-                this.setState({allProducts:result.data})
+                this.setState({allProducts:result.data});
+                var oldItems = JSON.parse(await localStorage.getItem('itemsArray')) || [];
+                if(oldItems.length!==0){
+                    var clickedItems=[];
+                    var quantityItems=[];
+                    oldItems.map((data)=>{
+                        quantityItems[data['product-id']]=data['product-quantity'];
+                        clickedItems[data['product-id']]='true';
+                    })
+                    this.setState({addClicked:clickedItems});
+                    this.setState({quantity:quantityItems});
+                }
             }
             else{
                 this.setState({isLoading:false})
@@ -35,37 +49,15 @@ class Home extends Component{
         });
     }
 
+    
+
     async addItem(id){
          var oldItems = JSON.parse(await localStorage.getItem('itemsArray')) || [];
-         var found=false;
-        if(oldItems.length!==0){
-            oldItems.map((data)=>{
-                if(data['product-id']===id){
-                    data['product-quantity']=parseInt(data['product-quantity'])+1;
-                    const dummy=data['product-pricePerUnit']*data['product-quantity'];
-                    data['product-total']=dummy;
-                    oldItems.splice(oldItems.indexOf(data),1);
-                    oldItems.push(data);
-                    localStorage.setItem('itemsArray', JSON.stringify(oldItems));
-                    found=true;
-                }
-                return null;
-            })
-            toast.success('Item Added', {
-            position: "top-right",
-            autoClose: 1000,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-        });
-        }
-        if(!found){
             var newItem = {
             'product-name': '',
             'product-price': '',
-            'product-priceOthers':'',
             'product-pricePerUnit':'',
+            'product-priceOthers':'',
             'product-quantity':'',
             'product-image':'',
             'product-id':'',
@@ -92,7 +84,66 @@ class Home extends Component{
                localStorage.setItem('itemsArray', JSON.stringify(oldItems));
             }
           })
-        } 
+          var clickedItems=[];
+          var quantityItems=[];
+          clickedItems=this.state.addClicked;
+          quantityItems=this.state.quantity;
+          clickedItems[id]='true';
+          quantityItems[id]='1';
+          this.setState({addClicked:clickedItems,quantity:quantityItems});
+    }
+
+    async increment(id){
+        var oldItems = JSON.parse(await localStorage.getItem('itemsArray')) || [];
+        oldItems.map((data)=>{
+
+            if(data['product-id']===id){
+                data['product-quantity']=parseInt(data['product-quantity'])+1;
+              var quantityItems=this.state.quantity;
+                quantityItems[data['product-id']]=data['product-quantity'];
+                this.setState({quantity:quantityItems});
+                const dummy=data['product-pricePerUnit']*data['product-quantity'];
+                    data['product-total']=dummy;
+                }
+                return null;
+        })
+        localStorage.removeItem('itemsArray');
+        localStorage.setItem('itemsArray', JSON.stringify(oldItems));
+        this.dispayCards();
+    }
+
+    async decrement(id){
+        var oldItems = JSON.parse(await localStorage.getItem('itemsArray')) || [];
+        oldItems.map((data)=>{
+            if(data['product-id']===id){
+                data['product-quantity']=parseInt(data['product-quantity'])-1;
+                var quantityItems=this.state.quantity;
+                quantityItems[data['product-id']]=data['product-quantity'];
+                this.setState({quantity:quantityItems});
+                const dummy=data['product-pricePerUnit']*data['product-quantity'];
+                    data['product-total']=dummy;
+                }
+                return null;
+        })
+        localStorage.removeItem('itemsArray');
+        localStorage.setItem('itemsArray', JSON.stringify(oldItems));
+        this.dispayCards();
+    }
+    
+    async remove(id){
+        var oldItems = JSON.parse(await localStorage.getItem('itemsArray')) || [];
+        oldItems.map((data)=>{
+            if(data['product-id']===id){
+                oldItems.splice(oldItems.indexOf(data),1);
+                var clickedItems=this.state.addClicked;
+                clickedItems[data['product-id']]='false';
+                this.setState({addClicked:clickedItems});
+                }
+                return null;
+        })
+        localStorage.removeItem('itemsArray');
+        localStorage.setItem('itemsArray', JSON.stringify(oldItems));
+        this.dispayCards();
     }
 
      dispayCards(){
@@ -107,7 +158,11 @@ class Home extends Component{
                                     <Card.Title><p className="homecardprodname">{data.name}<p style={{color:GLOBAL.BASE_COLORS.MEDIUM_PEACH}} className="homecardproddescription">{data.description}</p></p></Card.Title>
                                      {(data.price==="others")?<p className="homecardperunit">Price : ₹ {data.pricePerUnit} / {data.priceOthers}</p>
                                     :<p className="homecardperunit">Price : ₹ {data.pricePerUnit} / {data.price}</p>}
-                                    {(data.availability==="in-stock")?
+                                    {(this.state.addClicked[data.id]==='true')?
+                                      <div className="homequant">
+                                      <AiOutlinePlusSquare size="23" color="green" onClick={()=>{this.increment(data.id)}}/> {this.state.quantity[data.id]} <AiOutlineMinusSquare size="23" color="green"  onClick={()=>{this.state.quantity[data.id]>'1'?(this.decrement(data.id)):this.remove(data.id)}}/>
+                                      </div>:
+                                    (data.availability==="in-stock")?
                                         <Button value={data.id} variant="success addbutton" onClick={()=>{this.addItem(data.id);this.setState({Toastshow:true});setTimeout(()=>{this.setState({Toastshow:false})},500)}}>Add</Button>:
                                         <p className="outofstock" style={{color:'red',fontFamily:'roboto'}}>Out-Of-Stock</p>   
                                     }
@@ -121,10 +176,8 @@ class Home extends Component{
     } 
 
     render(){
-        
         return(
-            <Container className="bootstrapcontainer" >
-                   <ToastContainer />    
+            <Container className="bootstrapcontainer" >  
             <Header />
                 {(this.state.isLoading)?<div  style={{paddingTop:20,textAlign:'center'}}><Spinner animation="border" variant="success" /></div>:null}
                 {this.dispayCards()}
